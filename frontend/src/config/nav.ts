b1fae4bd736708fa100e1e630c302579d089e74e
Marcelo-teets <antunes.p.marcelo@gmail.com>
2026-04-10 15:53:37 -0300

    Update nav

--- frontend/src/config/nav.ts
+++ frontend/src/config/nav.ts
@@ -2,6 +2,7 @@ export const navItems = [
   ['/', 'Dashboard'],
   ['/search-profiles', 'Search Profiles'],
   ['/companies', 'Leads'],
+  ['/watch-lists', 'Watch Lists'],
   ['/monitoring', 'Monitoring Center'],
   ['/sources', 'Sources'],
   ['/agents', 'Agents Control'],
