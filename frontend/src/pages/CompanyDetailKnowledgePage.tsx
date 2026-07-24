6a55448e066a3ed90f14ec35a595aebe99668257
Marcelo-teets <antunes.p.marcelo@gmail.com>
2026-07-24 13:23:06 -0300

    feat: mount decision brief in company detail

--- frontend/src/pages/CompanyDetailKnowledgePage.tsx
+++ frontend/src/pages/CompanyDetailKnowledgePage.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { CompanyDecisionBriefPanel } from '../components/CompanyDecisionBriefPanel';
 import { CompanyKnowledgePanel } from '../components/CompanyKnowledgePanel';
 import { CompanySemanticEvidencePanel } from '../components/CompanySemanticEvidencePanel';
 import { CompanyDetailPage } from './CompanyDetailPage';
@@ -13,6 +14,9 @@ export function CompanyDetailKnowledgePage() {
       <div className="page company-knowledge-route-page">
         <section id="bloco-knowledge" style={{ scrollMarginTop: 64 }}>
           <CompanyKnowledgePanel companyId={id} />
+        </section>
+        <section id="bloco-briefing-decisorio" style={{ scrollMarginTop: 64 }}>
+          <CompanyDecisionBriefPanel companyId={id} />
         </section>
         <section id="bloco-evidencias-semanticas" style={{ scrollMarginTop: 64 }}>
           <CompanySemanticEvidencePanel companyId={id} />
